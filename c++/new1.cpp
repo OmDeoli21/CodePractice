c5a554fd337c5eaa02a027427af4cfe5850f0f24
OmDeoli21 <omdeoli06@gmail.com>
2026-02-22 03:02:38 +0530

    basic input in c++

---
+++ c++/new1.cpp
@@ -0,0 +1,78 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"hello world";
+//     return 0;
+// }
+
+
+////////////////////////////////////////////////////////////////////
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"this is " << "om";
+//     return 0;
+// }
+
+/////////////////////////////////////////////////////////////////////
+
+// /*    DATA TYPES   */
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int age = 19;
+//     float height = 172.6;
+//     char grade = 'A';
+//     double PI  = 3.14;
+//     cout<<"the age is"<< age
+//     <<" height is " << height
+//     <<" grade is "<< grade;
+//     return 0;
+// }
+
+////////////////////////////////////////////////////////////////////
+
+// /*      BOOLEAN      */
+// // 1  -  True 
+// // 0  -  False
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     bool isstudent = true;
+//     cout<<"user is student if output is 1 \n " << isstudent;
+//     return 0;
+// }
+
+/////////////////////////////////////////////////////////////////////
+
+// /*      typecasting (explicit)      */
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float price = 58.79;
+//     int newprice = (int)price;
+//     cout<<newprice;
+//     return 0;
+// }
+
+
+///////////////////////////////////////////////////////////////////////////
+
+// /*          INPUT          */
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout<<"enter your age : ";
+//     cin>>age;
+
+//     cout<<"You are "<<age << " years old";
+//     return 0;
+// }
+
+
+/////////////////////////////////////////////////////////////////////////////
