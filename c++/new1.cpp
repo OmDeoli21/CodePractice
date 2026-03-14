2c2d448b42b6b0e81359242cdd7ba8e510c00b5b
OmDeoli21 <omdeoli06@gmail.com>
2026-03-14 22:53:12 +0530

    Reverse Triangle pattern

--- c++/new1.cpp
+++ c++/new1.cpp
@@ -535,20 +535,44 @@
 
 
 //using 2 loops
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         int num =1;
+//         for(int j=0; j<i+1; j++){
+//             cout<<num<<" ";
+//             num++;
+//         }
+        
+//         cout<<endl;
+//     }
+//     return 0;
+// }
 
-#include<iostream>
-using namespace std;
-int main(){
+
+//////////////////////////
+
+/*      Reverse triangle pattern    */
+/*1
+  2 1
+  3 2 1
+  4 3 2 1*/
+
+  #include<iostream>
+  using namespace std;
+  int main(){
     int n;
     cin>>n;
+    int num=n;
     for(int i=0; i<n; i++){
-        int num =1;
-        for(int j=0; j<i+1; j++){
-            cout<<num<<" ";
-            num++;
+        for(int j=i+1; j<=i+1 && j>0; j--){
+          cout<<j<<" ";
         }
-        
         cout<<endl;
     }
+
     return 0;
-}
+  }
