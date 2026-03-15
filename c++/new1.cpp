81718e60df4f77a31fd02bc53b8d5498cbd52b89
OmDeoli21 <omdeoli06@gmail.com>
2026-03-15 23:29:21 +0530

    Floyds triangle - pattern printing

--- c++/new1.cpp
+++ c++/new1.cpp
@@ -561,17 +561,44 @@
   3 2 1
   4 3 2 1*/
 
+  // #include<iostream>
+  // using namespace std;
+  // int main(){
+  //   int n;
+  //   cin>>n;
+  //   int num=n;
+  //   for(int i=0; i<n; i++){
+  //       for(int j=i+1; j>0; j--){
+  //         cout<<j<<" ";
+  //       }
+  //       cout<<endl;
+  //   }
+
+  //   return 0;
+  // }
+
+
+  //////////////
+
+  /*    FLOYD'S TRIANGLE  
+  1
+  2 3
+  4 5 6
+  7 8 9 10
+  
+  */
+
   #include<iostream>
   using namespace std;
   int main(){
-    int n;
+    int n,num = 1;
     cin>>n;
-    int num=n;
     for(int i=0; i<n; i++){
-        for(int j=i+1; j<=i+1 && j>0; j--){
-          cout<<j<<" ";
-        }
-        cout<<endl;
+      for(int j=0; j<i+1; j++){
+        cout<<num<<" ";
+        num++;
+      }
+      cout<<endl;
     }
 
     return 0;
